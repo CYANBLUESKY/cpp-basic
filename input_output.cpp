c060c6d38b9c6d946e129f9069e011180396828f
cyans <187349603+CYANBLUESKY@users.noreply.github.com>
2025-02-13 20:33:23 +0900

    STUDY INPUT OUTPUT

--- input_output.cpp
+++ input_output.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
-int main()
-{
-    int a , b;
+int main() {
+    int a, b;
     std::cout << "Enter a number : "; // 문자열 출력
     std::cin >> a; // 사용자에게 정수를 입력박아 a에 저장
 
     std::cout << "Enter a number2 : "; // 문자열 출력
     std::cin >> b; // 사용자에게 정수를 입력받아 b에 저장
 
-    std::cout << "a + b = "<< a + b << std::endl; // 두 수의 합 출력
+    std::cout << "a + b = " << a + b << std::endl; // 두 수의 합 출력
     return 0;
 }
