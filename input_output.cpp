529147351f29603c244857587fead5e0ea0296a6
cyans <187349603+CYANBLUESKY@users.noreply.github.com>
2025-02-13 18:20:41 +0900

    STUDY INPUT OUTPUT

---
+++ input_output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int a , b;
+    std::cout << "Enter a number : ";
+    std::cin >> a;
+
+    std::cout << "Enter a number2 : ";
+    std::cin >> b;
+
+    std::cout << "a + b = "<< a + b << std::endl;
+    return 0;
+}
