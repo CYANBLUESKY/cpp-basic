47137e9ff75eb117e339742b49c9db8957ce72bb
cyans <187349603+CYANBLUESKY@users.noreply.github.com>
2025-02-14 10:09:07 +0900

    STUDY ASCII

--- print_ASCII.cpp
+++ print_ASCII.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include <windows.h>
-
 using namespace std;
 
 int main() {
-    SetConsoleOutputCP(65001);
     cout << "아스키 코드 출력하기 [32 ~ 126]:\n";
     for (char i = 32; i <= 126; i++) {
         cout << i << ((i % 16 == 15) ? '\n' : ' ');
